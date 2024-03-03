0238f3bf7b02a4f060903aedc2a92025ea918373
joek2021 <kellyjoseph18@gmail.com>
2024-03-03 14:37:14 +0000

    added basic product.html & products_details.html with products displaying (without images, to be fixed at a later date)

--- products/views.py
+++ products/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+from .models import Product
 
 # Create your views here.
 
-def Products(request):
-    return render(request, "products.html", )
+def all_products(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, "products/products.html", context )
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
